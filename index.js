9ac3f4aa02644b81a861d328954c187f5a38ca98
Rakibul1294 <md.rakibul.1294@gmail.com>
2020-06-01 21:03:38 +0600

    power-x-gym_back-end

--- index.js
+++ index.js
@@ -16,70 +16,70 @@ const uri = process.env.DB_PATH;
 let client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-app.get('/appointments', (req, res) => {
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("doctorPortal").collection("docAppointList");
-        collection.find().toArray((err, documents) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ message: err });
-            }
-            else {
-                res.send(documents);
-            }
-        });
-        // client.close();
-    });
-});
+// app.get('/appointments', (req, res) => {
+//     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("doctorPortal").collection("docAppointList");
+//         collection.find().toArray((err, documents) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 res.send(documents);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
 
-app.get('/appointedPeople', (req, res) => {
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("doctorPortal").collection("appointedPeople");
-        collection.find().toArray((err, documents) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ message: err });
-            }
-            else {
-                res.send(documents);
-            }
-        });
-        // client.close();
-    });
-});
+// app.get('/appointedPeople', (req, res) => {
+//     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("doctorPortal").collection("appointedPeople");
+//         collection.find().toArray((err, documents) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 res.send(documents);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
 
-app.get('/appointedPeople/:key', (req, res) => {
-    const key = req.params.key;
-    console.log(key);
+// app.get('/appointedPeople/:key', (req, res) => {
+//     const key = req.params.key;
+//     console.log(key);
 
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("doctorPortal").collection("appointedPeople");
-        collection.find({ joinAppointDate: key }).toArray((err, documents) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ message: err });
-            }
-            else {
-                console.log(documents);
-                res.send(documents);
-            }
-        });
-        // client.close();
-    });
-});
+//     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("doctorPortal").collection("appointedPeople");
+//         collection.find({ joinAppointDate: key }).toArray((err, documents) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 console.log(documents);
+//                 res.send(documents);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
 
 //delete
 //update
 // post
-app.post('/appointedPeople', (req, res) => {
-    const appointedPeople = req.body;
+app.post('/userDetails', (req, res) => {
+    const userDetails = req.body;
     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
     client.connect(err => {
-        const collection = client.db("doctorPortal").collection("appointedPeople");
-        collection.insert(appointedPeople, (err, result) => {
+        const collection = client.db("powerXGgym").collection("userDetails");
+        collection.insertOne(userDetails, (err, result) => {
             if (err) {
                 console.log(err);
                 res.status(500).send({ message: err });
@@ -92,27 +92,32 @@ app.post('/appointedPeople', (req, res) => {
     });
 });
 
-app.post('/prescriptionAdded/:key', (req, res) => {
-    const prescriptionAdded = req.body;
-    console.log(prescriptionAdded);
-    const key = req.params.key;
-    console.log(key);
-    // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-    client.connect(err => {
-        const collection = client.db("doctorPortal").collection("appointedPeople");
-        collection.updateOne({ "_id": ObjectID(key) }, { "$set": prescriptionAdded }, { upsert: true }, (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send({ message: err });
-            }
-            else {
-                // console.log(result);
-                res.send(result);
-            }
-        });
-        // client.close();
-    });
-});
+
+
+
+
+
+// app.post('/prescriptionAdded/:key', (req, res) => {
+//     const prescriptionAdded = req.body;
+//     console.log(prescriptionAdded);
+//     const key = req.params.key;
+//     console.log(key);
+//     // client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//     client.connect(err => {
+//         const collection = client.db("doctorPortal").collection("appointedPeople");
+//         collection.updateOne({ "_id": ObjectID(key) }, { "$set": prescriptionAdded }, { upsert: true }, (err, result) => {
+//             if (err) {
+//                 console.log(err);
+//                 res.status(500).send({ message: err });
+//             }
+//             else {
+//                 // console.log(result);
+//                 res.send(result);
+//             }
+//         });
+//         // client.close();
+//     });
+// });
 
 
 
